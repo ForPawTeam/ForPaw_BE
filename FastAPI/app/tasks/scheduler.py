4f4fe336de2129452c7cb86920e2346d8935cd2a
hoyaii <131665728+hoyaii@users.noreply.github.com>
2025-03-10 10:29:18 +0900

    fix: APScheduler가 작업을 실행할 때마다 해당 비동기 작업을 위한 새로운 이벤트 루프를 생성하도록 수정

--- FastAPI/app/tasks/scheduler.py
+++ FastAPI/app/tasks/scheduler.py
@@ -20,14 +20,25 @@ async def scheduled_update():
             else:
                 logger.error("최대 재시도 횟수 초과: 콘텐츠 기반 추천 정보 갱신 작업 실패")
 
+def run_async_task(coroutine):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        return loop.run_until_complete(coroutine)
+    finally:
+        loop.close()
+
 def start_scheduler():
     scheduler = AsyncIOScheduler(timezone="Asia/Seoul")
     trigger = IntervalTrigger(hours=1)
+
+    # 래퍼 함수를 사용하여 비동기 작업을 별도의 이벤트 루프에서 실행
     scheduler.add_job(
-        lambda: asyncio.create_task(scheduled_update()),
+        lambda: run_async_task(scheduled_update()),
         trigger=trigger,
         id="update_cb_data"
     )
+    
     try:
         scheduler.start()
         logger.info("스케줄러 시작 성공")
